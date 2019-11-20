75653542b04bafaf0f3536d7e2f6030e45a33c43
swon3210 <swon3210@gmail.com>
2019-11-20 23:03:28 +0900

    storyblock base structure

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,76 +3,39 @@ import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { withKnobs, text, boolean, object } from '@storybook/addon-knobs';
+
+
+// MARKDOWNS
 import test_markdown from '@/assets/notes/test.md'
 
-import MyButton from '../components/atoms/MyButton.vue'
 
+// BLOCKS
+import HeadlineImage from '@/components/blocks/headline_image.vue';
+import TextBlockWithImage from '@/components/blocks/text_block_with_image.vue';
 
-// 아하! / 만 넣으면 알아서 다 되는구나
-
-// 아하! action이 emit으로 전달되는걸 받아와서 출력하는거구나!
-
-const label = 'Styles';
-const defaultValue = {
-  backgroundColor: 'red'
-};
-const groupId = 'GROUP-ID1';
-
-const value = object(label, defaultValue, groupId);
-
-const testFunction = () => {
-  console.log('testFunction!')
-}
-
-storiesOf('components/atoms', module)
+storiesOf('components/blocks', module)
   .addDecorator(withKnobs)
-  .add('with text', () => ({
-    components: { MyButton },
+  .add('Headline Image', () => ({
+    components: { HeadlineImage },
     props: {
-      something: {
-        default: text('hey')
-      },
-      something2: {
-        default: boolean(false)
-      },
-      something3: {
-        default: object(label, defaultValue, groupId)
-      }
+      
     },
-    template: '<my-button @click="action">Hello Button</my-button>',
+    template: '<headline-image @click="action">Hello Button</headline-image>',
     methods: {
-      action: action('onMouseOver'),
+      
     },
   }), {
     notes: {markdown: test_markdown}
   })
-  // .add('with JSX', () => ({
-  //   components: { MyButton },
-  //   render() {
-  //     return <my-button onClick={this.action}>With JSX</my-button>;
-  //   },
-  //   methods: { action: linkTo('Button', 'with some emoji') }
-  // }))
-  // .add('with some emoji', () => ({
-  //   components: { MyButton },
-  //   template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-  //   methods: { action: action('clicked') }
-  // }))
-
-  storiesOf('components/molecules', module)
-  .addDecorator(withKnobs)
-  .add('with text', () => ({
-    components: { MyButton },
+  add('TextBlock With Image', () => ({
+    components: { TextBlockWithImage },
     props: {
-      something: {
-        default: text('hey')
-      },
-      something2: {
-        default: boolean(false)
-      }
+      
     },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action('clicked') },
+    template: '<text-block-with-image @click="action">Hello Button</text-block-with-image>',
+    methods: {
+      
+    },
   }), {
     notes: {markdown: test_markdown}
   })
